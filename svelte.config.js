60d44b747529fb452430b1251bc6d0bcfb67ee28
zachtsk <109352381+zachtsk@users.noreply.github.com>
2022-10-29 09:34:49 -0500

    Deploy to gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-		  postcss: true,
-		}),
-	  ],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            fallback: 'index.html'
+        }),
+        paths: {
+            base: "/retail-markdown-game"
+        }
+    }
 };
 
 export default config;
